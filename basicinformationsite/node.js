e1a32e8a22f5fb628326c48f25252d690c95b6c5
Faraz Khan <farazrk001@gmail.com>
2019-07-02 15:54:26 +0530

    Refactor as per assignment

--- basicinformationsite/node.js
+++ basicinformationsite/node.js
@@ -6,7 +6,7 @@ http.createServer((req, res) => {
 	console.log(req.url)
 	switch(req.url) {
 		case '/':
-			fs.readFile('./pages/index.html', (err, data) => {
+			fs.readFile('./index.html', (err, data) => {
 				res.writeHead(200, {'Content-Type': 'text/html'})
 				res.write(data)
 				console.log('something')
@@ -14,21 +14,21 @@ http.createServer((req, res) => {
 			})
 			break
 		case '/about':
-			fs.readFile('./pages/about.html', (err, data) => {
+			fs.readFile('./about.html', (err, data) => {
 				res.writeHead(200, {'Content-Type': 'text/html'})
 				res.write(data)
 				res.end()
 			})
 			break
 		case '/contact-me':
-			fs.readFile('./pages/contact-me.html', (err, data) => {
+			fs.readFile('./contact-me.html', (err, data) => {
 				res.writeHead(200, {'Content-Type': 'text/html'})
 				res.write(data)
 				res.end()
 			})
 			break
 		default:
-			fs.readFile('./pages/404.html', (err, data) => {
+			fs.readFile('./404.html', (err, data) => {
 				res.writeHead(200, {'Content-Type': 'text/html'})
 				res.write(data)
 				res.end()
